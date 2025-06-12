f5b17aec5bde8d8075bc42603529385a9011dce7
Syed Samiullah <sullah0490@gmail.com>
2025-06-12 16:26:35 +0500

    Finance Mobile App

---
+++ lib/Screens/Main_Dashboard/dashboard_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Dashboard_View extends StatelessWidget {
+  const Dashboard_View({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
